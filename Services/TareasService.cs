692a2a579c281a0cdd73f6bf3ca3649361a41595
Victor Alvarez <vaap2099@gmail.com>
2023-06-27 19:04:48 -0600

    implementacion servicios

---
+++ Services/TareasService.cs
@@ -0,0 +1,56 @@
+using webapi.Models;
+namespace webapi.Services;
+
+public class TareasService : ITareasService
+{
+    TareasContext context;
+
+    public TareasService(TareasContext dbcontext)
+    {
+        context = dbcontext;
+
+    }
+
+    public IEnumerable<Tarea> Get()
+    {
+        return context.Tareas;
+
+    }
+
+    public async Task Save (Tarea tarea)
+    {
+        context.Add(tarea);
+        await context.SaveChangesAsync();
+    }
+
+    public async Task Update (Guid id, Tarea tarea)
+    {
+        var taraeActual = context.Tareas.Find(id);
+
+        if (taraeActual != null)
+        {
+            taraeActual.Titulo = tarea.Titulo;
+            taraeActual.Descripcion = tarea.Descripcion;
+            taraeActual.PrioridadTarea = tarea.PrioridadTarea;
+            await context.SaveChangesAsync();
+        }
+        
+    }
+
+    public async Task Delete (Guid id)
+    {
+        var tareaActual = context.Tareas.Find(id);
+
+        if (tareaActual != null)
+        {
+            context.Remove(tareaActual);
+            await context.SaveChangesAsync();
+        }
+        
+    }
+
+}
+
+public interface ITareasService {
+
+}
